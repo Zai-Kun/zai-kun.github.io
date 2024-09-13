e5f7152c3d1622d33e262ab9b43e0d94fcb6c81e
Zai-Kun <122824602+Zai-Kun@users.noreply.github.com>
2024-09-13 10:14:18 +0500

    some changes

--- api/index.py
+++ api/index.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "hello", 200
+    return render_template("home.html")
